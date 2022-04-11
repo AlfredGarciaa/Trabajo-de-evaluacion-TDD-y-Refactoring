102730f5ef27f81a73b5961ae07350ab9c9bda64
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-04-11 18:17:54 -0400

    9) Interfaz de Usuario - Intermedia

--- src/presenter.js
+++ src/presenter.js
@@ -3,13 +3,19 @@ import Vacas_Toros from './Vacas_Toros'
 const form = document.querySelector("#vista-1-form");
 const div = document.querySelector("#visualizaciones");
 const inputNumero_Caracteres = document.querySelector("#numero-caracteres");
+const inputNumero_Intentos = document.querySelector("#numero-intentos");
+
 let vacas_Toros = new Vacas_Toros();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let numero_Caracteres = inputNumero_Caracteres.value;
-  vacas_Toros.definir_Numero_Caracteres(numero_Caracteres);
+  let numero_Intentos = inputNumero_Intentos.value;
 
-  div.innerHTML = "<p>" + vacas_Toros.getNumero_Caracteres() + "</p>";
+  vacas_Toros.definir_Numero_Caracteres(numero_Caracteres);
+  vacas_Toros.definir_Numero_Intentos(numero_Intentos);
+
+  div.innerHTML =  `<p> ${vacas_Toros.getNumero_Caracteres()} </p>
+                    <p> ${vacas_Toros.getNumero_Intentos()} </p>`;
 });
