37a5f099ee86df889fc3204232ebdd7d3fed157a
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-04-11 15:46:04 -0400

    Refactorizacion del 1)

--- src/Vacas_Toros.js
+++ src/Vacas_Toros.js
@@ -1,6 +1,13 @@
-function definir_Numero_Caracteres(numero)
+let numero_Caracteres;
+
+function getNumero_Caracteres()
 {
-  return numero;
+    return numero_Caracteres;
 }
 
-export default definir_Numero_Caracteres;
+function definir_Numero_Caracteres(numero_Car)
+{
+    numero_Caracteres = numero_Car;
+}
+
+export {definir_Numero_Caracteres, getNumero_Caracteres};
--- src/Vacas_Toros.test.js
+++ src/Vacas_Toros.test.js
@@ -1,10 +1,11 @@
-import definir_Numero_Caracteres from './Vacas_Toros.js'
+import {definir_Numero_Caracteres, getNumero_Caracteres} from './Vacas_Toros.js'
 
 describe(" ******** JUEGO DE VACAS Y TOROS ******* ", () =>
  {
     it("1.- Si se ingresa el número 4 como número de caracteres, mostrar el número 4.", () =>
     {
-        let respuesta = definir_Numero_Caracteres(4);
+        definir_Numero_Caracteres(4)
+        let respuesta = getNumero_Caracteres();
         expect(respuesta).toEqual(4);
     });
 });
