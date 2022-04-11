1e29ec32602550cecba98bc913de9f4ecd9e9472
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-04-11 17:07:34 -0400

    Refactorizacion del 5)

--- src/Vacas_Toros.js
+++ src/Vacas_Toros.js
@@ -1,35 +1,40 @@
-let numero_Caracteres;
-let numero_Intentos;
-const limite_Inferior = 1;
-const limite_Superior = 10;
-
-function getNumero_Caracteres()
+class Vacas_Toros
 {
-    return numero_Caracteres;
-}
-
-function definir_Numero_Caracteres(numero_Car)
-{
-    numero_Caracteres = numero_Car;
-    controlar_Cantidad_Caracteres();
-}
-  
-function controlar_Cantidad_Caracteres()
-{
-  if ((numero_Caracteres < limite_Inferior) || (numero_Caracteres > limite_Superior))
+  constructor()
   {
-    numero_Caracteres = "Número incorrecto de caracteres. Intente otra vez por favor";
+      this.numero_Caracteres;
+      this.numero_Intentos;
+      this.limite_Inferior = 1;
+      this.limite_Superior = 10;
+  }
+
+  definir_Numero_Caracteres(numero_Car)
+  {
+      this.numero_Caracteres = numero_Car;
+      this.controlar_Cantidad_Caracteres();
+  }
+
+  controlar_Cantidad_Caracteres()
+  {
+      if ((this.numero_Caracteres < this.limite_Inferior) || (this.numero_Caracteres > this.limite_Superior))
+      {
+          this.numero_Caracteres = "Número incorrecto de caracteres. Intente otra vez por favor";
+      }
+  }
+
+  definir_Numero_Intentos(numero_Int)
+  {
+      this.numero_Intentos = numero_Int;
+  }
+
+  getNumero_Caracteres()
+  {
+      return this.numero_Caracteres;
+  }
+
+  getNumero_Intentos(){
+      return this.numero_Intentos;
   }
 }
 
-function definir_Numero_Intentos(numero_Int)
-{
-  numero_Intentos = numero_Int;
-}
-
-function getNumero_Intentos()
-{
-  return numero_Intentos;
-}
-
-export {definir_Numero_Caracteres, getNumero_Caracteres, definir_Numero_Intentos, getNumero_Intentos};
+export default Vacas_Toros;
--- src/Vacas_Toros.test.js
+++ src/Vacas_Toros.test.js
@@ -1,4 +1,4 @@
-import {definir_Numero_Caracteres, getNumero_Caracteres, definir_Numero_Intentos, getNumero_Intentos} from './Vacas_Toros.js'
+import Vacas_Toros from './Vacas_Toros.js'
 
 describe(" ******** JUEGO DE VACAS Y TOROS ******* ", () =>
  {
@@ -7,40 +7,42 @@ describe(" ******** JUEGO DE VACAS Y TOROS ******* ", () =>
             // TDD -> REFACTORIZACION
                 //FUNCIONALIDADES
      // GIT (Commits) -> GIT-HUB (Repositorio) -> GIT-ACTIONS (Pull Request en Main)
-    let respuesta;
+    
+     let vacas_Toros = new Vacas_Toros();
+     let respuesta;
     
     it("1.- Si se ingresa el número 4 como número de caracteres, mostrar el número 4.", () =>
     {
-        definir_Numero_Caracteres(4);
-        respuesta = getNumero_Caracteres();
+        vacas_Toros.definir_Numero_Caracteres(4);
+        respuesta = vacas_Toros.getNumero_Caracteres();
         expect(respuesta).toEqual(4);
     });
 
     it("2) Si se ingresa el número 0 como número de caracteres, mostrar el mensaje de <Número incorrecto de caracteres. Intente otra vez por favor>.", () => 
     {
-        definir_Numero_Caracteres(0);
-        respuesta = getNumero_Caracteres();
+        vacas_Toros.definir_Numero_Caracteres(0);
+        respuesta = vacas_Toros.getNumero_Caracteres();
         expect(respuesta).toEqual("Número incorrecto de caracteres. Intente otra vez por favor");
     });
 
     it("3) Si se ingresa el número -1 como número de caracteres, mostrar el mensaje de <Número incorrecto de caracteres. Intente otra vez por favor>.", () => 
     {
-        definir_Numero_Caracteres(-1);
-        respuesta = getNumero_Caracteres();
+        vacas_Toros.definir_Numero_Caracteres(-1);
+        respuesta = vacas_Toros.getNumero_Caracteres();
         expect(respuesta).toEqual("Número incorrecto de caracteres. Intente otra vez por favor");
     });
 
     it("4) Si se ingresa el número 11 como número de caracteres, mostrar el mensaje de <Número incorrecto de caracteres. Intente otra vez por favor>.", () => 
     {
-        definir_Numero_Caracteres(11);
-        respuesta = getNumero_Caracteres();
+        vacas_Toros.definir_Numero_Caracteres(11);
+        respuesta = vacas_Toros.getNumero_Caracteres();
         expect(respuesta).toEqual("Número incorrecto de caracteres. Intente otra vez por favor");
     });
 
     it("5) Si se ingresa el número 4 como número máximo de intentos, mostrar el número 4.", () =>
      {
-        definir_Numero_Intentos(5);
-        respuesta = getNumero_Intentos();
+        vacas_Toros.definir_Numero_Intentos(5);
+        respuesta = vacas_Toros.getNumero_Intentos();
         expect(respuesta).toEqual(5);
     });
 });
